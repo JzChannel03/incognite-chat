895ba0de4ae1fcc0300541d2a35e2a8a0089f2ce
Jose Ventura <jzchannel03@outlook.com>
2024-10-07 13:56:05 -0400

    improves in chat preview, input for messages added

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,28 +1,28 @@
+import { Input } from "@nextui-org/react";
 import Body from "../components/body";
 import Header from "../components/header";
 import MobileNav from "../components/mobile-nav";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 function ChatBubble({ isMine }: Readonly<{ isMine: boolean }>) {
   return (
-    <div
-      className={`p-1 bubble-chat-bg w-fit text-wrap inline-block rounded-md ${
-        isMine ? "float-left" : "float-right"
-      }`}
-    >
-      <span>Hola Hermanito!</span>
+    <div className={`flex w-full ${isMine ? "flex-row-reverse" : "flex-row"}`}>
+      <span className="p-2 rounded-md w-fit text-wrap bubble-chat-bg">
+        Hola Hermanito!
+      </span>
     </div>
   );
 }
 
 function ChatPreview() {
   return (
-    <section className="w-4/6 h-full border-l-1 border-solid border-gray-200 grid grid-rows-[60px,20fr]">
-      <div className="px-5 py-3 text-white flex align-middle items-center">
+    <section className="border-gray-200 grid grid-rows-[60px,1fr,80px] border-l-1 border-solid w-4/6 h-full">
+      <div className="flex items-center px-5 py-3 text-white align-middle">
         ChatHeaderPreview
       </div>
       <div
         /* style={{ backgroundImage: `url(${chatBg})` }} */
-        className={`bg-white w-full h-full flex flex-col-reverse shrink-0 basis-auto p-5 bg-[image:var(--image-url)]`}
+        className={`bg-white w-full h-full flex gap-2 flex-col-reverse shrink-0 basis-auto p-5 bg-[image:var(--image-url)]`}
       >
         <ChatBubble isMine={true} />
         <ChatBubble isMine={false} />
@@ -33,20 +33,31 @@ function ChatPreview() {
         <ChatBubble isMine={false} />
         <ChatBubble isMine={true} />
       </div>
+      <div className="flex flex-row justify-center items-center gap-2 bg-red-400 p-5 w-full h-full">
+        <Icon
+          className="group-hover:scale-125 transition duration-300"
+          width={30}
+          icon="majesticons:microphone"
+        />
+        <Input placeholder="Escribe tu mensaje..." />
+        <button className="bg-blue-gradient p-2 rounded-2xl text-white">
+          Enviar
+        </button>
+      </div>
     </section>
   );
 }
 
 const Home = () => {
   return (
-    <section className="w-full h-full md:flex md:flex-row overflow-hidden">
-      <div className="relative w-full min-w-80 h-full md:w-2/6 grid grid-rows-[130px,20fr]">
+    <section className="md:flex md:flex-row w-full h-full overflow-hidden">
+      <div className="relative grid grid-rows-[130px,20fr] w-full md:w-2/6 min-w-80 h-full">
         <Header></Header>
         <Body></Body>
         <MobileNav></MobileNav>
       </div>
       <ChatPreview></ChatPreview>
-      {/* <button className="bg-blue-gradient p-2 rounded-2xl fixed bottom-20 right-3 text-white">
+      {/* <button className="right-3 bottom-20 fixed bg-blue-gradient p-2 rounded-2xl text-white">
         Generate AI
       </button> */}
     </section>
